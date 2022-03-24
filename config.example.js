144ad88d02bc70ae146f568d0f04fa7099e1d5f1
killbinvlog <killbinvlog@gmail.com>
2022-03-24 20:45:58 +0100

    Update of the MongoDB connection configuration

--- config.example.js
+++ config.example.js
@@ -6,8 +6,11 @@ export default {
 		startupMessage: false,
 	},
 	mongodbConnectOptions: {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
+		autoIndex: false,
+		maxPoolSize: 10,
+		serverSelectionTimeoutMS: 5000,
+		socketTimeoutMS: 45000,
+		family: 4,
 	},
 	imageUploader: {
 		cacheTimeMs: 15 * 60 * 1000,
