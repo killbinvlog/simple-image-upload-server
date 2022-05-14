cd1bd66ddae53661c62b0de5b9983f51621ea347
killbinvlog <killbinvlog@gmail.com>
2022-05-14 13:21:08 +0200

    Fixed a problem with the MongoDB connection

--- database/connectDb.js
+++ database/connectDb.js
@@ -3,7 +3,7 @@ import config from '../config.js';
 
 export default function connectDb() {
 	return new Promise((resolve, reject) => {
-		mongoose.connect(process.env.MONGODB_URI, config.mongodb_connect_options);
+		mongoose.connect(process.env.MONGODB_URI, config.mongodbConnectOptions);
 		mongoose.Promise = global.Promise;
 		mongoose.connection.once('error', reject);
 		mongoose.connection.once('connected', resolve);
